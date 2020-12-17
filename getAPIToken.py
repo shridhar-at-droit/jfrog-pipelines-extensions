b474266b2cd40ee397d67f672574d2404adf89cb
Shridhar <36041359+shridharMe@users.noreply.github.com>
2020-12-17 10:53:15 +0000

    first commit

---
+++ getAPIToken.py
@@ -0,0 +1,20 @@
+import requests 
+import http.cookies
+import requests
+import json
+import sys
+import os
+import json 
+
+username = 'xxxx' #add username
+password = 'yyyyy' #add password
+
+req = requests.get('http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)', auth=(username,password))
+crumb=req.text.split(":")
+headers = {crumb[0]: crumb[1]}
+url = 'http://localhost:8080/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken'
+payload = {'newTokenName': 'my-token'}
+
+r = requests.post(url, data=json.dumps(payload), auth=(username,password), headers=headers, cookies=req.cookies)
+data = json.loads(r.text)
+print(data['data']['tokenValue'])
