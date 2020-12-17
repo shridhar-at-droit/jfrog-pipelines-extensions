94aa137541e1b750816d0510bce10629d0d2170d
Shridhar <36041359+shridharMe@users.noreply.github.com>
2020-12-17 12:19:59 +0000

    added some comments

--- getAPIToken.py
+++ getAPIToken.py
@@ -4,12 +4,14 @@ import json
 username = 'xxxx' #add username
 password = 'yyyyy' #add password
 
+# create a crumb token
 req = requests.get('http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)', auth=(username,password))
 crumb=req.text.split(":")
 headers = {crumb[0]: crumb[1]}
-url = 'http://localhost:8080/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken'
-payload = {'newTokenName': 'my-token'}
+params = {'newTokenName': 'portal-dev'}
 
-r = requests.post(url, data=json.dumps(payload), auth=(username,password), headers=headers, cookies=req.cookies)
+#generate api token
+url = 'http://localhost:8080/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken'
+r = requests.post(url, params=params, auth=(username,password), headers=headers, cookies=req.cookies)
 data = json.loads(r.text)
 print(data['data']['tokenValue'])
